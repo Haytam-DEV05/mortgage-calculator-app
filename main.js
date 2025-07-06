4082e593695b29d978ef23df7e645e5f1aaa3ea6
haytam <haytam@gmail.com>
2025-07-06 12:28:20 +0100

    finish the project

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
+const btnCalculate = document.querySelector('.btn-calculate')
+var box2s = document.querySelectorAll('.box2')
 
-const btnCalculate = document.querySelector(".btn-calculate") ;
-var box2s = document.querySelectorAll(".box2") ;
+let amount = document.getElementById('amount')
+let years = document.getElementById('years')
+let rate = document.getElementById('rate')
 
 
-let amount = document.getElementById("amount") ;
-let years = document.getElementById("years") ;
-let rate = document.getElementById("rate") ;
 
-btnCalculate.addEventListener("click",()=>{
-    box2s.forEach((box)=>{
-        box.classList.toggle("notActive") ;
-    })
-    calculatResulte(amount.value,years.value,rate.value)
-    setTimeout(()=>{window.location.reload()},2000)
+
+btnCalculate.addEventListener('click', () => {
+  if(!checkError()){
+    return ;
+  }
+  calculatResulte(amount.value, years.value, rate.value)
+
+
+  box2s.forEach(box => {
+      box.classList.toggle('notActive')
+  })
+  setTimeout(() => {window.location.reload()}, 2000)
+
 })
-    
-function calculatResulte(a,b,c){
-    var resulte = (a * b) / c ;
-    document.querySelector(".resulte-monthly").innerHTML = `$${resulte.toFixed(2)}` ;
-    document.querySelector(".resulte-total").innerHTML = `$${resulte.toFixed(2) * 3}` ;
+
+function calculatResulte (a, b, c) {
+  var resulte = (a * b) / c
+  document.querySelector('.resulte-monthly').innerHTML = `$${resulte.toFixed(
+    2
+  )}`
+  document.querySelector('.resulte-total').innerHTML = `$${
+    resulte.toFixed(2) * 3
+  }`
 }
+
+
+function checkError(){
+  var inputs = document.querySelectorAll(`input[type="text"]`) ;
+  inputs.forEach((input)=>{
+    if(input.value === ""){
+      input.classList.add("error") ;
+      input.previousElementSibling.classList.add("error") ;
+      var parent = input.parentElement ;
+      parent.nextElementSibling.classList.add("active") ;
+      return false;
+    }else{
+      input.classList.remove("error") ;
+      input.previousElementSibling.classList.remove("error") ;
+      var parent = input.parentElement ;
+      parent.nextElementSibling.classList.remove("active") ;
+    }
+  })
+}
+
+
+
+
+
